5805cd4e6f8f6a19050454130e8ea21cda50ed4f
donaya365 <82857286+donaya365@users.noreply.github.com>
2022-10-05 16:25:24 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# INSTALL ? COPY HERE
+
+<pre><code>apt update && apt upgrade -y && apt install -y wget screen && wget -q https://zeaking && chmod +x setup.sh && screen -S setup ./setup.sh</code></pre>
---
+++ about.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m      ⇱ SUCKER PROJECT ⇲                      \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# NABI MUHAMMAD                                 #"
+echo -e "# KELUARGA KITA                                 #"
+echo -e "# TEMAN & GURU                                  #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Created By : HAMBA ALLAH                      #"
+echo -e "# Contact    : SILAKAN SHOLAWAT                 #"
+echo -e "# Whatsapp   : SEDEKAH YANG BANYAK              #"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m      ⇱ SUCKER PROJECT ⇲                      \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
